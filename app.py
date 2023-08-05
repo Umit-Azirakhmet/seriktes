17052eb5c4cc7f069a63874ed748a65f939f3cf2
Umit-Azirakhmet <umit.azirakhmet@nu.edu.kz>
2023-08-06 05:39:23 +0600

    change in prompt

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
-import openai
+
+# import openai
 from dotenv import load_dotenv
-import pickle
+
+# import pickle
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
